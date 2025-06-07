f7ee1fd154cf8e5ec5853dfce795dcfa73509d86
kennps <napat.samretvit@gmail.com>
2025-06-07 12:59:16 +0700

    Add head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,35 @@
+export default function Head() {
+  return (
+    <>
+      <title>TWS</title>
+      <meta
+        name="description"
+        content="Information about The Wall Survival [SRP] server"
+      />
+
+      <meta property="og:title" content="TWS" />
+      <meta
+        property="og:description"
+        content="Information about The Wall Survival [SRP] server"
+      />
+      <meta
+        property="og:image"
+        content="https://countdown-tws.vercel.app/images/og-image.png"
+      />
+      <meta property="og:url" content="https://countdown-tws.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="TWS" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="TWS" />
+      <meta
+        name="twitter:description"
+        content="Information about The Wall Survival [SRP] server"
+      />
+      <meta
+        name="twitter:image"
+        content="https://countdown-tws.vercel.app/images/og-image.png"
+      />
+    </>
+  );
+}
